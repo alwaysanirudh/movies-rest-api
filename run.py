708f1faa6a59c43da1c564a6ef5992bff41273d4
Anirudh Parui <anirudh@ambab.com>
2015-05-27 00:40:17 +0530

    Added app_port settings

--- run.py
+++ run.py
@@ -2,9 +2,9 @@ import os
 from eve import Eve
 from eve.auth import BasicAuth
 
-# Heroku support: bind to PORT if defined, otherwise default to 5000.
-if 'PORT' in os.environ:
-    port = int(os.environ.get('PORT'))
+# Heroku support: bind to APP_PORT if defined, otherwise default to 5000.
+if 'APP_PORT' in os.environ:
+    port = int(os.environ.get('APP_PORT'))
     # use '0.0.0.0' to ensure your REST API is reachable from all your
     # network (and not only your computer).
     host = '0.0.0.0'
--- settings.py
+++ settings.py
@@ -4,8 +4,8 @@ import os
 # Heroku, sensible DB connection settings are stored in environment variables.
 MONGO_HOST = os.environ.get('MONGO_HOST', 'localhost')
 MONGO_PORT = os.environ.get('MONGO_PORT', 27017)
-MONGO_USERNAME = os.environ.get('MONGO_USERNAME', 'user')
-MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD', 'user')
+MONGO_USERNAME = os.environ.get('MONGO_USERNAME', 'imdb')
+MONGO_PASSWORD = os.environ.get('MONGO_PASSWORD', 'imdb')
 MONGO_DBNAME = os.environ.get('MONGO_DBNAME', 'imdb')
 
 DEBUG = True
